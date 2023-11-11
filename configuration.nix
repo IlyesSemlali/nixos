7f04130e7f14b4de6df21e4c5ec633d513bf5d07
Ilyès Semlali <ilyes@linux>
2023-11-11 21:55:20 +0100

    feat(users): set zsh as default shell

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  users.defaultUserShell = pkgs.zsh;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ilyes = {
     isNormalUser = true;
