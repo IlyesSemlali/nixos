3605683aea1798e23fe2b91834ffe4b988b4a6fc
Ilyès Semlali <ilyes@linux>
2023-11-20 12:29:17 +0100

    feat(nixos): configure GC and auto update

--- configuration.nix
+++ configuration.nix
@@ -8,14 +8,9 @@
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      ./nixos.nix
       ./desktop-environments/gnome.nix
     ];
-
-  # Add support for flakes
-  nix.package = pkgs.nixFlakes;
-  nix.extraOptions = ''
-     experimental-features = nix-command flakes
-  '';
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -158,13 +153,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
 
 }
