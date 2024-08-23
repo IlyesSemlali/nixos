3849b1d1447aaa2032568f8f72ae183ed1b7dda7
Ilyès Semlali <ilyes@linux>
2024-08-23 22:37:59 +0000

    chore(nix): rework gnome config

--- nixos/desktop-environments/gnome.nix
+++ nixos/desktop-environments/gnome.nix
@@ -1,25 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
 
-  # Gnome
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+    };
+
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "fr";
+      variant = "";
+    };
+  };
+
   environment.gnome.excludePackages = (with pkgs; [
       gnome-photos
       gnome-tour
-      cheese # webcam tool
-      evince # document viewer
+      cheese
+      evince
       gnome.gnome-characters
   ]);
+
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "";
-  };
 }
