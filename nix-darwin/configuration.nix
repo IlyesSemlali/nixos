6c5b0caa72832cbd8056a93707f5c891f1aee2b3
Ilyès Semlali <ilyes@macbook>
2025-06-07 20:17:28 +0200

    feat(macos): properly link applications

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ self, pkgs, config, ... }:
 
 {
   system = {
@@ -7,6 +7,28 @@
 
     # Set Git commit hash for darwin-version.
     configurationRevision = self.rev or self.dirtyRev or null;
+
+    # Properly alias (link) installed applications
+    # so macOS can find them anywhere:
+    activationScripts.applications.text = let
+      env = pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+      };
+    in
+      pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+          '';
   };
 
   nix = {
